9689304931fbdc1b66984a6dd30e99aaeac2e238
CyrilFauveau <fauveaucyril@gmail.com>
2019-11-20 21:46:00 +0100

    add php form test 4

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['message'])) {
+
+  $name = $_POST['name'];
+  $mail = $_POST['mail'];
+  $message = $_POST['message'];
+
+  $to      = 'fauveaucyril@gmail.com';
+  $subject = $mail;
+  $headers = 'From: webmaster@example.com' . "\r\n" .
+  'Reply-To: webmaster@example.com' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+
+  mail($to, $subject, $message, $headers);
+
+  echo 'test';
+}
