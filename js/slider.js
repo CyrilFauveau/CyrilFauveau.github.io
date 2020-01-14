fc61c7e1a725adac5fa7ffed779fab77bd38aaff
CyrilFauveau <fauveaucyril@gmail.com>
2020-01-14 21:23:08 +0100

    correct alt slide images

--- js/slider.js
+++ js/slider.js
@@ -2,18 +2,21 @@ var futuroid = {
 	"title" : "Futuroid",
 	"image" : "img/futuroid.png",
 	"text" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	"alt" : "Futuroid",
 }
 
 var perioseries = {
 	"title" : "PerioSeries",
 	"image" : "img/perioseries.png",
 	"text" : "PerioSeries est un projet fictif réalisé en binome avec <a href='http://neooo.fr/'>Néo Roussel</a> lors de ma 1ere année à MyDigitalSchool. Ce projet a été assez important pour moi puisque c'est un des premiers sites sur lequel j'ai commencer à coder en PHP. J'ai également pu dévouvrir les bases de données et la manipulation de celles-ci, ce qui fut un réel plaisir et ce qui m'a pousser à continuer dans ce domaine.",
+	"alt" : "PerioSeries",
 }
 
 var sevenkeys = {
 	"title" : "Seven Keys",
 	"image" : "img/seven_keys.jpg",
 	"text" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	"alt" : "Seven Keys",
 }
 
 var slider = [futuroid, perioseries, sevenkeys];
@@ -24,7 +27,7 @@ function setSlide() {
 	document.getElementById('slideTitle').innerHTML = slider[actualSlide].title;
 	document.getElementById('slideText').innerHTML = slider[actualSlide].text;
 	document.getElementById('slideImage').src = slider[actualSlide].image;
-	document.getElementById('slideImage').alt = slider[actualSlide].text;
+	document.getElementById('slideImage').alt = slider[actualSlide].alt;
 }
 
 
